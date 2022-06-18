63c554e6fff2e8e7a250480ce908bbb894e086f7
diggy63 <daniel.kanczugowski@gmail.com>
2022-06-18 15:02:19 -0700

    finished pop coloring

--- create_map.py
+++ create_map.py
@@ -5,16 +5,37 @@ data = pandas.read_csv("Volcanoes.csv")
 
 lat = list(data["LAT"])
 lon = list(data["LON"])
-v_name = list(data["NAME"])
-fg = folium.FeatureGroup(name='my_map')
+elv = list(data["ELEV"])
+name = list(data["NAME"])
 
-for lt,ln,n in zip(lat,lon,v_name):
-    fg.add_child(folium.Marker(location=[int(lt), int(ln)], popup=n, icon=folium.Icon(color='red')))
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
 
 map = folium.Map(location=[47.645260156417585, -
                  122.32721740454191], zoom_start=6)
+fg = folium.FeatureGroup(name = "My Map")
+
+def colorElev(el):
+    if el < 1000:
+        return "green"
+    elif el > 1000 and el < 3000:
+        return "orange"
+    else:
+        return "red"
+ 
+for lt, ln, el, name in zip(lat, lon, elv, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    el_color = colorElev(el)
+    fg.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(iframe), radius = 6, fill_color=el_color, color="grey", fill_opacity = 0.7))
 
 
+fg.add_child(folium.GeoJson(data = open("world.json", "r", encoding="utf-8-sig").read(), 
+style_function=lambda x: {"fillColor":"green" if x['properties']['POP2005'] < 10000000
+ else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 
 map.add_child(fg)
